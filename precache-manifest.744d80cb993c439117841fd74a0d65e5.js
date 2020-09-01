a00ce9ddec2d8cc688527d79f2b164b7620d5953
Big Doofus Man <baileyak@vcu.edu>
2020-08-31 21:04:56 -0700

    Updates

---
+++ precache-manifest.744d80cb993c439117841fd74a0d65e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d844e77d02bb94357bb4bd0bbb4c9d",
+    "url": "/machi-koro/index.html"
+  },
+  {
+    "revision": "435b182fd499d77b7e5b",
+    "url": "/machi-koro/static/css/main.3e8074db.chunk.css"
+  },
+  {
+    "revision": "8d102364b13b096beed2",
+    "url": "/machi-koro/static/js/2.39f2e50c.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/machi-koro/static/js/2.39f2e50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435b182fd499d77b7e5b",
+    "url": "/machi-koro/static/js/main.1c301936.chunk.js"
+  },
+  {
+    "revision": "f4a004727fa6edf4d878",
+    "url": "/machi-koro/static/js/runtime-main.feb2d336.js"
+  }
+]);
